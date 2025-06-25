a3beb275c7bba6efc647a543339bdfbeca950335
aminehamidi <hamidii.aminee@gmail.com>
2025-06-25 17:23:25 +0200

    add modify password

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordUserForm;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AccountController extends AbstractController
@@ -12,5 +17,21 @@ final class AccountController extends AbstractController
     public function index(): Response
     {
         return $this->render('account/index.html.twig');
+    }
+
+    #[Route('/modifier-mon-mot-de-passe', name: 'app_account_modify_pwd')]
+    public function password(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManagerInterface): Response
+    {
+        $user=$this->getUser();
+        $form = $this->createForm(PasswordUserForm::class, $user, [
+            'passwordHasher' => $userPasswordHasher
+        ]);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManagerInterface->flush();
+        }
+        return $this->render('account/password.html.twig',[
+            'modifyPwd' => $form->createView()
+        ]);
     }
 }
